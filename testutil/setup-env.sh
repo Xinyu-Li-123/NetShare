3daa21e2aa58922084b8a19cb0befe5a177d03b3
Eiger Li <eigerli@outlook.com>
2023-01-02 18:44:59 -0500

    update env setup script

--- testutil/setup-env.sh
+++ testutil/setup-env.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd $HOME
 
-VIRTUAL_ENV=NetShare
-USERNAME=xinyu
+VIRTUAL_ENV=NetShare 
+USERNAME=xinyu 
 CONDA_EXEC=$HOME/anaconda3/bin/conda
 NETSHARE_LOCAL_REPO=/nfs/NetShare
 
@@ -18,16 +18,18 @@ fi
 eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
 conda init
 
-if [ -f $CONDA_EXEC ] 
+# create virtual environment if not exists
+if ! { conda env list | grep $VIRTUAL_ENV; } >/dev/null 2>&1
 then
-    echo "Anaconda3 installed."
+    echo "Conda environment $VIRTUAL_ENV not installed."
+    conda create -y --name $VIRTUAL_ENV python=3.6
 else
-    echo "Anaconda3 not installed. Start installation now..."
-    wget https://repo.anaconda.com/archive/Anaconda3-2022.05-Linux-x86_64.sh
-    bash Anaconda3-2022.05-Linux-x86_64.sh -b -p $HOME/anaconda3
+    echo "Conda environment $VIRTUAL_ENV installed."
 fi
-eval "$($HOME/anaconda3/bin/conda shell.bash hook)"
-conda init
+source $HOME/anaconda3/etc/profile.d/conda.sh
+conda activate $VIRTUAL_ENV
+# if python<3.6.1, pyparsing will throw exception
+conda install python=3.6.1
 
 # If already cloned
 if ! [ -d $NETSHARE_LOCAL_REPO]
@@ -38,3 +40,11 @@ then
 else
     echo "$NETSHARE_LOCAL_REPO exists! Skip git clone..."
 fi
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+ray start --head && ray stop
+
+cd $NETSHARE_LOCAL_REPO
+pip3 install cmake
+pip3 install -e .
